0129aabefaac2f329c1ec488886e4eef012771bf
Esiowu <esiowufranklyn@gmail.com>
2022-12-12 05:49:31 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,15 +16,15 @@ last_digit = n % 10;
 printf("Last digit of %d is %d", n, last_digit);
 if (last_digit > 5)
 {
-puts("and is greater than 5");
+puts(" and is greater than 5");
 }
 else if (last_digit == 0)
 {
-puts("and is 0");
+puts(" and is 0");
 }
 else
 {
-puts("and is less than 6 and not 0");
+puts(" and is less than 6 and not 0");
 }
 return (0);
 }
