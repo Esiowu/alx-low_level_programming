4e217980bc8d78c9587f16daba7ef6333778c3bb
Esiowu <esiowufranklyn@gmail.com>
2022-12-12 06:38:24 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,13 +7,11 @@
 
 int main(void)
 {
-	char i;
-
-	for (i = 'a'; i <= 'z'; i++)
-
-	{
-		putchar(i);
-	}
-	putchar('\n');
-	Return (0);
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
 }
