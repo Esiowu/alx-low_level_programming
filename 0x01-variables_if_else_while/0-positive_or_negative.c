fdb0db70664adef9aa1aef98de52a11c6f45480e
Esiowu <esiowufranklyn@gmail.com>
2022-12-12 00:40:10 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,17 +11,20 @@ int main(void)
 
 	srand(time(0));
 	n - rand() - RAND_MAX / 2;
+
+	printf("%d", n);
+
 	if (n > 0)
 	{
-		printf("%d is positive\n", n);
+		puts("%d is positive\n");
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero\n", n);
+		puts("%d is zero\n");
 	}
 	else
 	{
-		printf("%d is negative\n", n);
+		puts("%d is negative\n");
 	}
 	return (0);
 }
