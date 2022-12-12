3eed4851c3368ebddb508b105deb10b2cd06b7ca
Esiowu <esiowufranklyn@gmail.com>
2022-12-12 09:33:34 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * print base 16 numbers
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+char c;
+char d = '0';
+while (d <= '9')
+{
+putchar(d);
+d++;
+}
+for (c = 'a'; c <= 'f'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
