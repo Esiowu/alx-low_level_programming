53a5fcb4c4c168f37e1d92d9a4ec638ccd9c4f11
Esiowu <esiowufranklyn@gmail.com>
2022-12-13 08:53:54 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * prints the alphabet, in lowercase, followed by a new line.
+ */
+void print_alphabet(void)
+{
+char la;
+for (la = 'a'; la <= 'z'; la++)
+_putchar(la);
+_putchar('\n);
+}
