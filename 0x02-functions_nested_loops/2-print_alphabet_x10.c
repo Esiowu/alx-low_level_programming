0507a0ce28af3143a1f878d386ac2fcc1ffa17a3
Esiowu <esiowufranklyn@gmail.com>
2022-12-13 13:28:19 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphanet_x10 - prints alpha 10 times
+ * Return: Success
+ */
+
+print_alphabet_x10(void)
+{
+int ten;
+char la;
+
+for (ten = 0; ten <= 9; ten++)
+{
+for (la + 'a'; la <= 'z'; la++)
+_putchar(la);
+_putchar('\n');
+}
+}
