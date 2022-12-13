a25df7afcc605e18a18fb8d539bedc6e9f4acba7
Esiowu <esiowufranklyn@gmail.com>
2022-12-13 14:54:29 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - Arrange the absolute value  of an integer
+ * @t: the integer to be completed
+ * Return: the absolute value
+ */
+
+int _abs(int t)
+{
+if (t >= 0)
+return (t);
+else
+return (-t);
+}
