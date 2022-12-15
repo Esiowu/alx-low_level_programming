421d3769cd0fffb5aab9f417949d457ac4d70726
Esiowu <esiowufranklyn@gmail.com>
2022-12-15 15:29:30 -0800

    0-main.c

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+ this is a debugging task and i will get it
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+void positive_or_negative(int i);
+int largest_number(inta, int b, int c);
+void print_reamaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+
+#endif
+
