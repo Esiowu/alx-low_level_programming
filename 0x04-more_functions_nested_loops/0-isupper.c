69e98bcb753fdd3a350a31f5c84fd9c509098441
Esiowu <esiowufranklyn@gmail.com>
2022-12-15 12:49:45 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _1supper - A function that checks for upper character.
+ * @c: input charcter.
+ * Return: 1 if is an upper character, 0 in other case.
+ */
+int _1supper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
