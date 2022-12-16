a5de9dbc10714f6520279b8819562ecebab94067
Esiowu <esiowufranklyn@gmail.com>
2022-12-15 19:26:19 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_number - prints number from 0 - 9
+ * Return: void
+ */
+
+void print_number(void)
+{
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
