5a6984ab036cd6004282c292dfba47062484c0e3
Esiowu <esiowufranklyn@gmail.com>
2022-12-15 19:19:30 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiples two integers
+ * @a:The first number
+ * @b: the second number
+ * Return: return the result
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
