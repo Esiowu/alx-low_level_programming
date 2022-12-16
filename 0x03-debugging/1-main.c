49f9594fc2201d88c30162c4a17457f5e5334cf4
Esiowu <esiowufranklyn@gmail.com>
2022-12-15 20:34:26 -0800

    0-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
+
 /**
  * main - causes and infinite loop
  * Return: 0
- */
+*/
+
 int main(void)
 {
 int i;
+
 printf("Infinite loop incoming :(\n");
+
 i = 0;
+
 /**
  * while (i < 10)
  * {
  * putchar(i);
  * }
  */
+
 printf("infinite loop avoided! \\o/\n");
+
 return (0);
 }
