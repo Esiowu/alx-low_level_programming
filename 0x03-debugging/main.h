f332634b68b6a3bff06a9f1da6adff3fa7d11554
Esiowu <esiowufranklyn@gmail.com>
2022-12-15 16:19:04 -0800

    main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 void positive_or_negative(int i);
-int largest_number(inta, int b, int c);
+int largest_number(int a, int b, int c);
 void print_reamaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 
