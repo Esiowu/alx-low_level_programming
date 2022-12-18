1f8c9c9694974df9ce344df946be7a12562f8490
Esiowu <esiowufranklyn@gmail.com>
2022-12-18 15:39:35 -0800

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,11 +15,14 @@ else
 int i, j;
 
 for (i = 0; i < n; i++)
-
+{
+for (j = 0; j < n; j++)
 {
 if (j == i)
 _putchar('\\');
-else if _putchar(' ');
+else if (j < i)
+_putchar(' ');
+}
 _putchar('\n');
 }
 }
