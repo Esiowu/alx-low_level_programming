c888d1d4cacf70eb8691304963914382c705bd14
Esiowu <esiowufranklyn@gmail.com>
2022-12-20 04:28:27 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the numbers of the integers
+ * @a: int a
+ * @b: int b
+ */
+void swap_int(int *a, int *b)
+{
+	int a:
+
+	q = *a;
+	*a = *b;
+	*b = q;
+}
