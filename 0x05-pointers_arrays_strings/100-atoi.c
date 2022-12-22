9facd423d3751792e33ec09d2c512a5563d4f64c
Esiowu <esiowufranklyn@gmail.com>
2022-12-22 04:56:11 -0800

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _atoi - a function that convert a string to an integer
+ * @s: An input string
+ * Return: integer from conversion
+ */
+
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int total = 0;
+	cha null_flag = 0;
+
+	while (*s)
+	{
+		if (*s == '-')
+			sign *= -1;
+
+		if (*s >= '0' && *s <= '9')
+		{
+			null_flag = 1;
+			total = total * 10 + *s - '0';
+		}
+
+		else if (null_flag)
+			break;
+		s++;
+	}
+
+	if (sign < 0)
+		total = (-total);
+	return (total);
+}
