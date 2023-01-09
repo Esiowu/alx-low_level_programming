e3934b5a89e8acdc87dda3b4127b9bffb3f9599d
Esiowu <esiowufranklyn@gmail.com>
2023-01-09 10:37:14 -0800

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * _length - calculates the length
+ * @s: The string to be checked.
+ * Return: length of string.
+ */
+
+int _length(char *s)
+{
+	if (*s != '\0')
+	{
+		s++;
+		return (1 + _length(s));
+
+	}
+	return (0);
+
+}
+
+/**
+ * _check - checks the string
+ * @str: strings to check
+ * @i: iterator
+ * @n: number
+ * Return: 0 or 1
+ */
+
+int _check(char *str, int n, int i)
+{
+	if (i >= n)
+		return (1);
+	if (str[1] == str[n])
+		return (_check(str, n - 1, i + 1));
+
+	return (0);
+}
+
+/**
+ * is_palindrome - shows if string is palindrome or not
+ * @s: string
+ * Return: 0 or 1
+ */
+
+int is_palindrome(char *s)
+{
+int n = _length(s);
+int i = 0;
+
+return (_check(s, n - 1, i));
+}
