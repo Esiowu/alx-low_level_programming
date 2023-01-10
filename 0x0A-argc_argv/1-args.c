1650b9192679cd1b4573ff50ca2062f0382436a7
Esiowu <esiowufranklyn@gmail.com>
2023-01-10 01:08:50 -0800

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdib.h>
+
+/**
+ * main - a program that prints number of arguments
+ * @argc: first parametr
+ * @argv: second parameter
+ * Description: prints number of argument
+ * Return: 0 for code success
+ */
+
+int main(int argc, char **argv)
+{
+	int sum;
+
+	for (sum = 0; sum < argc; sum++)
+	{
+		*argv[sum] = *argv[sum];
+	}
+	printf("%d\n", (sum - 1));
+	return (0);
+}
